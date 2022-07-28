996175a62acb4bf232817b8cd0c646dc5f1bf1b4
islafranco <isla_franco@icloud.com>
2022-07-27 21:06:09 -0700

    adding more recipes

--- models/recipieSeed.js
+++ models/recipieSeed.js
@@ -10,40 +10,54 @@ module.exports = [
         name: 'Lemon Blueberry Bread With Glaze',
         description: 'With fresh blueberries and lemons are bright and glorious combination when baked into a loaf of bread and drizzled with a lemon glaze.',
         img: 'https://therealfooddietitians.com/wp-content/uploads/2021/06/Gluten-Free-Lemon-Blueberry-Bread-with-Lemon-Glaze-4.jpg',
+        link: 'https://therealfooddietitians.com/gluten-free-lemon-blueberry-bread-with-glaze/',
     },
     {
         name: 'Banana Bread With Chocolate Chips',
         description: 'Soft and tender with just enough sweetness, made with less sugar for just the right texture. Serve warm or smear your favorite nut butter.',
         img: 'https://therealfooddietitians.com/wp-content/uploads/2021/05/Gluten-Free-Banana-Bread-with-Chocolate-Chips-3.jpg',
+        link: 'https://therealfooddietitians.com/gluten-free-banana-bread/',
     },
     {
         name: 'Tuna Noodle Cassarole',
         description: 'a healthy tuna noodle casserole made from scratch, look no further! This lightened up tuna noodle casserole is made with a creamy, light parmesan mushroom sauce, noodles, canned tuna and peas.',
         img: 'https://busybuthealthy.com/wp-content/uploads/2018/02/Tuna-Noodle-Casserole_1.jpg',
+        link: 'https://www.ambitiouskitchen.com/healthy-tuna-noodle-casserole/',
     },
     {
         name: 'Crisy Air Fryer Chicken Tenders',
         description: 'Crispy and juicy on the inside, flavored with garlic and parsley, these make the perfect snack or appetizer!',
         img: 'https://www.dishbydish.net/wp-content/uploads/Air-Fryer-Chicken-Tenders-Gluten-Free-Dairy-Free_Final5-scaled.jpg',
+        link: 'https://www.dishbydish.net/air-fryer-chicken-tenders/',
     },
     {
         name: 'Best Chocloate Chip Cookies',
         description: 'The Best gluten free chocolate chip cookies, perfectly soft, chewy, golden, and irresistable!',
         img: 'https://meaningfuleats.com/wp-content/uploads/2018/06/IMG_1244-2-500x500.jpg',
+        link: 'https://www.glutenfreefollowme.com/recipe/chocolate-chip-cookies/',
     },
     {
         name: 'Fettuccine Alfredo',
         description: 'Homemade Fetticcune with chicken or shrimp, the perfect pasta craving!',
         img: 'https://www.cottercrunch.com/wp-content/uploads/2021/07/Fettuccine-gluten-free-chicken-shrimp-vegetarian-low-carb-options_.5-scaled.jpg',
+        link: 'https://www.cottercrunch.com/healthy-fettuccine-gluten-free/',
     },
     {
         name: 'Lemon Dill Salmon',
         description: 'Dead simple salmon cooked right in foil, with no clean up!',
         img: 'https://s23209.pcdn.co/wp-content/uploads/2016/08/Lemon-Dill-Salmon-in-FoilIMG_1418edit.jpg',
+        link: 'https://fitfoodiefinds.com/oven-baked-salmon-in-foil/',
     },
     {
         name: 'Pumpkin Pie with Crust',
         description: 'Easy, delicious pumpkin pie, with homeade flaky, buttery gluten free pie crust!',
         img: 'https://divascancook.com/wp-content/uploads/2015/11/gluten-free-pumpkin-pie-recipe.jpg',
+        link: 'https://divascancook.com/easy-gluten-free-pumpkin-pie-recipe-crust/',
     },
+    {
+        name: 'Sourdough Gluten Free Bread',
+        description: 'Simple 4 ingridient Sourdough Bread recipe is perfect start with if you are new to sourdough bread baking.',
+        img: 'https://www.kitchenstewardship.com/wp-content/uploads/2018/04/gluten-free-sourdough-croissant-from-Chris-Stafferton.jpg',
+        link: 'https://www.whattheforkfoodblog.com/2020/05/17/gluten-free-sourdough-bread-recipe/',
+    }
 ]
